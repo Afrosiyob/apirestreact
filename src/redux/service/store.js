d79e31bdfc2bd036b30427d02096f0be07e1c993
Afrosiyob <afrosiyob.a97@gmail.com>
2021-09-29 23:06:33 +0500

    add
    add

--- src/redux/service/store.js
+++ src/redux/service/store.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, compose, createStore } from "redux";
-
+import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from "redux-saga";
 
 import rootSaga from "../sagas";
@@ -17,7 +17,10 @@ if ( process.env.NODE_ENV === `development` ) {
     middlewares.push( logger );
 }
 
-export const store = compose( applyMiddleware( ...middlewares ) )( createStore )( rootReducer );
+export const store = compose( composeWithDevTools(
+    applyMiddleware( ...middlewares )
+    // other store enhancers if any
+) )( createStore )( rootReducer );
 
 
 sagaMiddleware.run( rootSaga );
